12b6139bd881a7594835b4408ee026ae0449abaf
yoshiyahoo <josiahtroup@gmail.com>
2021-01-23 16:24:07 -0800

    Added Control Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,71 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class CONTROLS {
+        public static final int JOYSTICK_PORT = 0;
+        public static final byte ARCADE_JOYSTICK_PORT = 1;
+
+        public static final class JOYSTICK {
+            public static final int TRIGGER = 1;  // BUTTON
+            public static final int THUMB = 2;    // BUTTON
+
+            public static final int THROTTLE = 3; // AXIS
+            public static final int X = 0;        // AXIS
+            public static final int Y = 1;        // AXIS
+            public static final int Z = 2;        // AXIS
+
+            public static final class TOP {
+                public static final int RIGHT_TOP = 6;  // BUTTON
+                public static final int RIGHT_BOT = 4;  // BUTTON
+                public static final int LEFT_TOP = 5;   // BUTTON
+                public static final int LEFT_BOT = 3;   // BUTTON
+            }
+
+            public static final class SIDE {
+                public static final int BUTTON_7 = 7;   // BUTTON
+                public static final int BUTTON_8 = 8;   // BUTTON
+                public static final int BUTTON_9 = 9;   // BUTTON
+                public static final int BUTTON_10 = 10; // BUTTON
+                public static final int BUTTON_11 = 11; // BUTTON
+                public static final int BUTTON_12 = 12; // BUTTON
+            }
+
+            public static final class POV {
+                public static final int ID = 0;          // POV
+                public static final int TOP = 0;         // POV
+                public static final int TOP_RIGHT = 45;  // POV
+                public static final int RIGHT = 90;      // POV
+                public static final int RIGHT_BOT = 135; // POV
+                public static final int BOT = 180;       // POV
+                public static final int LEFT_BOT = 225;  // POV
+                public static final int LEFT = 270;      // POV
+                public static final int LEFT_TOP = 315;  // POV
+            }
+        }
+
+        public static final class ARCADE {
+            public static final int A = 1;        // BUTTON
+            public static final int B = 2;        // BUTTON
+            public static final int X = 3;        // BUTTON
+            public static final int Y = 4;        // BUTTON
+            public static final int L1 = 5;       // BUTTON
+            public static final int R1 = 6;       // BUTTON
+            public static final int TOP_LEFT = 8; // BUTTON
+
+            public static final int R2 = 3;       // AXIS
+            public static final int L2 = 2;       // AXIS
+
+            public static final class POV {
+                public static final int ID = 0;          // POV
+                public static final int TOP = 0;         // POV
+                public static final int TOP_RIGHT = 45;  // POV
+                public static final int RIGHT = 90;      // POV
+                public static final int RIGHT_BOT = 135; // POV
+                public static final int BOT = 180;       // POV
+                public static final int LEFT_BOT = 225;  // POV
+                public static final int LEFT = 270;      // POV
+                public static final int LEFT_TOP = 315;  // POV
+            }
+        }
+    }
 }
